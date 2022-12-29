dd50c254d072337aa1bfa3230ee89070222ae0ba
Daniel Eades <danieleades@hotmail.com>
2022-12-29 17:17:31 +0000

    fixup

--- src/cqrs.rs
+++ src/cqrs.rs
@@ -158,8 +158,7 @@ where
     ///
     /// async fn do_something(cqrs: MyFramework) -> Result<(),AggregateError<MyUserError>>  {
     ///     let command = MyCommands::DoSomething;
-    ///     let mut metadata = HashMap::new();
-    ///     metadata.insert("time".to_string(), chrono::Utc::now().to_rfc3339());
+    ///     let metadata = HashMap::from([("time".to_string(), chrono::Utc::now().to_rfc3339())]);
     ///
     ///     cqrs.execute_with_metadata("agg-id-F39A0C", command, metadata).await
     /// }
